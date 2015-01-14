6cddca3de6812cb7f1a8d4bbdcdb5c803da59324
Christian D. Madsen <sevenplagues@gmail.com>
2015-01-13 22:24:59 -0700

    made note-offs more thorough
    oldschool MIDI gear ought to have way fewer sticky-notes now

--- extrovert-self-notefuncs.lua
+++ extrovert-self-notefuncs.lua
@@ -6,9 +6,11 @@ return {
 
 		for chan, notes in pairs(self.sustain) do
 			if next(notes) ~= nil then -- Check for active note-sustains within the channel before trying to act upon them
-				for note, dur in pairs(notes) do
+				for note, tab in pairs(notes) do
 				
-					self.sustain[chan][note] = math.max(0, dur - 1) -- Decrease the relevant duration value
+					local dur = tab.sust
+
+					self.sustain[chan][note].sust = math.max(0, dur - 1) -- Decrease the relevant duration value
 				
 					if dur == 0 then -- If the duration has expired...
 						self:noteParse({chan, 128, note, 127, 0}) -- Parse a noteoff for the relevant channel and note
@@ -73,7 +75,7 @@ return {
 					if v.target == "note" then -- If the target is NOTE, modify the NOTE value based on the dial's position
 						note[3] = math.max(0, math.min(127, note[3] + v.bottom + math.floor(v.breadth * self.dial[k])))
 					elseif v.target == "velocity" then -- If the target is VELOCITY, modify the VELOCITY value based on the dial's position
-						note[4] = math.max(0, math.min(127, note[4] + v.bottom + math.floor(v.breadth * self.dial[k])))
+						note[4] = math.max(1, math.min(127, note[4] + v.bottom + math.floor(v.breadth * self.dial[k])))
 					end
 				end
 			end
@@ -82,7 +84,11 @@ return {
 
 		if rangeCheck(note[2], 128, 159) then -- If this is a NOTE-ON or NOTE-OFF command, modify the contents of the MIDI-sustain table
 		
-			local sust = self.sustain[note[1]][note[3]] or -1 -- If the corresponding sustain value isn't nil, copy it to sust; else set sust to -1
+			-- If the corresponding sustain value isn't nil, copy it to sust; else leave sust set to -1
+			local sust = -1
+			if self.sustain[note[1]][note[3]] ~= nil then
+				sust = self.sustain[note[1]][note[3]].sust
+			end
 		
 			if note[2] == 144 then -- For ON-commands, increase the note's global duration value by the incoming duration amount, if applicable
 				sust = math.max(note[5], sust)
@@ -91,9 +97,20 @@ return {
 			end
 			
 			if sust == -1 then -- If the sustain was nil and a note-ON didn't occur, or if a note-off occurred, set the sustain to nil
+
+				-- If multiple note-ons are being represented by the same sustain, then send multiple note-offs to compensate.
+				-- Some MIDI devices require this, or else get sticky notes.
+				while self.sustain[note[1]][note[3]].count > 1 do
+					self.sustain[note[1]][note[3]].count = self.sustain[note[1]][note[3]].count - 1
+					self:noteSend(note)
+				end
+
 				self.sustain[note[1]][note[3]] = nil
-			else -- If a note-ON occurred, set the relevant sustain to the note's duration value
-				self.sustain[note[1]][note[3]] = sust
+
+			else -- If a note-ON occurred, set the relevant sustain to the note's duration value, and increase the sustain-count
+				self.sustain[note[1]][note[3]] = self.sustain[note[1]][note[3]] or {sust = 0, count = 0}
+				self.sustain[note[1]][note[3]].sust = sust
+				self.sustain[note[1]][note[3]].count = self.sustain[note[1]][note[3]].count + 1
 			end
 			
 		end
